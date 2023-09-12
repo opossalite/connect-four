08cdc65c7e3fd1debb66518ddd52da8928589339
TheTerrior <werbird10@gmail.com>
2023-09-11 18:06:12 -0700

    Added contiguous board memory

--- src/internal/engine.go
+++ src/internal/engine.go
@@ -1,17 +1,63 @@
-package internal 
+package internal
+
+
+// Return state of the engine after each turn.
+type State int32
+const (
+    Ok State = iota //dropped piece
+    Invalid //column full
+    Win //win
+)
+
 
 // Stores the Connect Four board.
 //
 // Uses float32 instead of int8 to remove type casting for neural networks.
 type Board struct {
-    TilesRed []float32
-    TilesYellow []float32
+    TilesRed *[42]float32
+    TilesYellow *[42]float32
+    WinTiles *[168]int32
 }
 
-//func test() {
-//}
+
+// Drop a red piece onto the board at the specified column.
+func (board Board) DropRed(column int32) (State, int32) {
+    if board.TilesRed[column*7] != 0 { //column completely filled
+        return Invalid, 0
+    }
+    var tile = drop(board.TilesRed, board.TilesYellow, column)
+    //win detection here
+    return Ok, tile
+}
 
 
+// Drop a yellow piece onto the board at the specified column.
+func (board Board) DropYellow(column int32) (State, int32) {
+    if board.TilesYellow[column*7] != 0 { //column completely filled
+        return Invalid, 0
+    }
+    var tile = drop(board.TilesYellow, board.TilesRed, column)
+    //win detection here
+    return Ok, tile
+}
+
+
+// Handles piece dropping regardless of color. Returns the dropped tile's location.
+func drop(board_main *[42]float32, board_secondary *[42]float32, column int32) int32 {
+    var main_slice = board_main[column*6:column*6+6]
+    var secondary_slice = board_secondary[column*6:column*6+6]
+    for i := int32(1); i < 6; i++ {
+        if main_slice[i] != 0 { //tile above is open
+            main_slice[i-1] = 1
+            secondary_slice[i-1] = -1
+            return column*6 + i-1
+        }
+    }
+    // column was completely open
+    main_slice[5] = 1
+    secondary_slice[5] = -1
+    return column*6 + 5
+}
 
 
 
--- src/main.go
+++ src/main.go
@@ -1,15 +1,84 @@
 package main
 
 import "fmt"
+import "unsafe"
 import "connect-four/internal"
 
 func main() {
     internal.Bridge()
 
-    y := make([]float32, 42)
-    z := make([]float32, 42)
-    x := internal.Board{y, z}
-    fmt.Println(x.TilesRed[0])
+    //y := make([]float32, 42)
+    //z := make([]float32, 42)
+    //y := [42]float32{}
+    //z := [42]float32{}
+    //a := [168]int32{}
+    var board_block = [252]int32{} //allocates enough contiguous memory for our board
+    var tiles_red = (*[42]float32)(unsafe.Pointer(&board_block[0]))
+    var tiles_yellow = (*[42]float32)(unsafe.Pointer(&board_block[42]))
+    var tiles_win = (*[168]int32)(unsafe.Pointer(&board_block[84]))
+    board := internal.Board{TilesRed: tiles_red, TilesYellow: tiles_yellow, WinTiles: tiles_win}
+
+    var state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    state, tile = board.DropRed(0)
+    if state == internal.Invalid {
+        fmt.Println("Invalid drop!")
+        return
+    }
+    fmt.Printf("Dropped into %d\n", tile)
+
+    ////fmt.Println(fmt.Parse("Dropped into {{.tile}}"))
+    //fmt.Printf("Red: %f\n", board.TilesRed)
+    //fmt.Println("Red: ", board.TilesRed)
+    //fmt.Println("Red1: ", board.TilesRed[5])
+    //fmt.Printf("Yellow: %f\n", board.TilesYellow)
+
+    //b := [10]float32{}
+    //c := &b
+    //d := c[2:5]
+    ////(*c)[0] = 1
+    //d[0] = 1
+    //fmt.Printf("Test: %f\n", b)
 }
 
 
