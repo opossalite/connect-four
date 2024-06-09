5ecdec8f760ea0152d88dead21ea0e106c070a96
opossalite <werbird10@gmail.com>
2024-06-09 07:44:18 +0000

    Finished first rendition of optimized connect four win verification

--- src/board.rs
+++ src/board.rs
@@ -1,8 +1,8 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{cell::RefCell, mem::MaybeUninit, rc::Rc};
 
 
 
-
+/// Represents the result of placing a tile on the board
 pub enum Event {
     Nothing,
     Invalid,
@@ -10,16 +10,17 @@ pub enum Event {
 }
 
 
+/// Struct for one tile, of which a board contains 42
 #[derive(Clone)]
 pub struct Tile {
-    //pub update: bool,
     pub color: bool,
-    pub group: Rc<Box<u8>>,
+    pub groups: [Rc<RefCell<u8>>; 3],
+    pub vertical: i32,
 }
 
 
+/// Main struct used for Connect Four
 pub struct Board {
-    //pub groups: HashMap<u8, Rc<Box<u8>>>,
     pub tiles: [[Option<Tile>; 6]; 7],
 }
 impl Board {
@@ -32,13 +33,7 @@ impl Board {
     }
 
 
-    fn update_groups(&mut self, color: bool, cur: (usize, usize), left: (usize, usize), right: (usize, usize)) -> Event {
-
-        Event::Nothing
-
-    }
-
-
+    /// Drop a piece into the board and return the result
     pub fn place(&mut self, color: bool, column: usize) -> Event {
 
         let mut found = None;
@@ -58,60 +53,32 @@ impl Board {
 
         let row;
         match found {
-            None => return Event::Invalid,
+            None => return Event::Invalid, //this column is completely filled up
             Some(i) => row = i,
         }
 
-        //let mut search = [true; 6];
+        // first handle vertical wins
+        let vertical_count;
+        if row > 0 {
+            vertical_count = self.tiles[column][row-1].as_ref().unwrap().vertical + 1;
 
-        //if column == 0 { //on left side
-        //    search[0] = false;
-        //    search[1] = false;
-        //    search[2] = false;
-        //} else if column == 6 { //on right side
-        //    search[3] = false;
-        //    search[4] = false;
-        //    search[5] = false;
-        //}
+            // handle vertical win
+            if vertical_count > 3 {
+                return Event::Win;
+            }
 
-        //if row == 0 { //on bottom
-        //    search[2] = false;
-        //    search[5] = false;
-        //} else if row == 5 { //on top
-        //    search[0] = false;
-        //    search[3] = false;
-        //}
+        } else { //first one in the column
+            vertical_count = 1;
+        }
 
-        //let left_tile = &mut self.tiles[column+1][row-1];
-        //let right_tile = &mut self.tiles[column-1][row+1];
+        // prepare for group management
+        const COORDS: [((i32, i32), (i32, i32)); 3] = [((-1, 1), (1, -1)), ((-1, 0), (1, 0)), ((-1, -1), (1, 1))];
+        let mut groups: [Rc<RefCell<u8>>; 3] = unsafe { [MaybeUninit::uninit().assume_init(), MaybeUninit::uninit().assume_init(), MaybeUninit::uninit().assume_init()] };
+        let mut win = false;
 
-        // ul and dr
-        //let code: u8;
-        //if !search[0] { //left side is dead, only go right side
-        //    code = match &mut self.tiles[column+1][row-1] {
-        //        Some(tile) => if tile.color == color { 2 } else { 0 },
-        //        None => 0,
-        //    };
-
-        //} else if !search[5] { //right side is dead, only go left side
-        //    code = match &mut self.tiles[column-1][row+1] {
-        //        Some(tile) => if tile.color == color { 1 } else { 0 },
-        //        None => 0,
-        //    };
-
-        //} else { //both sides are open
-        //    let left_code = match &mut self.tiles[column-1][row+1] {
-        //        Some(tile) => if tile.color == color { 1 } else { 0 },
-        //        None => 0,
-        //    };
-        //    let right_code = match &mut self.tiles[column+1][row-1] {
-        //        Some(tile) => if tile.color == color { 2 } else { 0 },
-        //        None => 0,
-        //    };
-        //    code = left_code + right_code;
-        //}
-
-        for ((l_col, l_row), (r_col, r_row)) in [((-1, 1), (1, -1)), ((-1, 0), (1, 0)), ((-1, -1), (1, 1))] {
+        // iterate through all directions
+        for i in 0..COORDS.len() {
+            let ((l_col, l_row), (r_col, r_row)) = COORDS[i];
 
             // first figure out where we can check without out-of-bounds errors
             let mut search = [true; 6];
@@ -139,6 +106,7 @@ impl Board {
             let right_col = (column as i32 + r_col) as usize;
             let right_row = (row as i32 + r_row) as usize;
 
+            // figure out which code we are dealing with, which update case
             let code: u8;
             if !search[0] { //left side is dead, only go right side
                 code = match &mut self.tiles[right_col][right_row] {
@@ -167,47 +135,56 @@ impl Board {
             // handle codes 0, 1, 2, 3
             match code {
                 0 => { //create new group, join neither
-
+                    groups[i] = Rc::new(RefCell::new(1)); //set initial size of group to 1
                 },
                 1 => { //join left
-
+                    let left_group = self.tiles[left_col][left_row].as_ref().unwrap().groups[i].clone();
+                    *left_group.borrow_mut() += 1; //increment group size
+                    groups[i] = left_group; //add this tile to the group
                 },
                 2 => { //join right
-
+                    let right_group = self.tiles[right_col][right_row].as_ref().unwrap().groups[i].clone();
+                    *right_group.borrow_mut() += 1; //increment group size
+                    groups[i] = right_group; //add this tile to the group
                 },
                 3 => { //unify both sides
+                    let left_group = self.tiles[left_col][left_row].as_ref().unwrap().groups[i].clone();
+                    let right_group_size = *self.tiles[right_col][right_row].as_ref().unwrap().groups[i].borrow_mut();
+                    let summed_size = *left_group.borrow_mut() + right_group_size + 1; //figure out new size
+                    *left_group.borrow_mut() = summed_size; //increment group size
+
+                    // update both ends of the former right group to be in the new unified left group
+                    let far_right_col = (r_col * right_group_size as i32 + column as i32) as usize;
+                    let far_right_row = (r_row * right_group_size as i32 + row as i32) as usize;
+                    self.tiles[right_col][right_row].as_mut().unwrap().groups[i] = left_group.clone(); //update close end
+                    self.tiles[far_right_col][far_right_row].as_mut().unwrap().groups[i] = left_group.clone(); //update far end
+
+                    // add this tile to the group
+                    groups[i] = left_group;
+
+                    // check for win
+                    if summed_size > 3 {
+                        win = true;
+                    }
 
                 },
                 _ => unreachable!(),
             }
-            
+
+            // handle win condition
+            if win {
+                return Event::Win;
+            }
         }
 
-        // handle codes 0, 1, 2, 3
+        // now that updates have been handled in all directions, create the new tile
+        let tile = Tile {
+            color,
+            groups: [groups[0].clone(), groups[1].clone(), groups[2].clone()],
+            vertical: vertical_count,
+        };
+        self.tiles[column][row] = Some(tile);
 
-        //match code {
-        //    0 => {
-
-        //    },
-        //    1 => {
-
-        //    },
-        //    2 => {
-
-        //    },
-        //    3 => {
-
-        //    },
-        //    _ => unreachable!(),
-        //}
-
-
-        todo!();
-
-
-        
-
-        // this column is completely filled up
         Event::Nothing
     }
 }
